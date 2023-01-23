c2051347b12439b99d3e1687b984ae06c85dbc68
Stefan Bussemaker <stefan.bussemaker@kadaster.nl>
2023-01-23 14:59:58 +0100

    refactor: cleanup old code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 
 @Controller()
-export class AppController {
-  @Get()
-  getHello(): string {
-    return `
-      <h1>Mock APIs - Solid Quest</h1>
-      <p>Welkom bij de overheids mock APIs</p>
-      <p><a href='/brp/login'>Login BRP</a></p>
-      <p><a href='/brk/login'>Login Kadaster</a></p>
-      `;
-  }
-}
+export class AppController {}
