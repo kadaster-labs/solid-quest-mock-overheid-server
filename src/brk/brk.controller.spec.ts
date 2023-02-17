64caff8871a07fe9bee03ee7cc15b415256f7f16
Stefan Bussemaker <stefan.bussemaker@kadaster.nl>
2023-02-17 11:52:47 +0100

    ci: added checks to ci

--- src/brk/brk.controller.spec.ts
+++ src/brk/brk.controller.spec.ts
@@ -1,5 +1,13 @@
+import { ConfigService } from '@nestjs/config';
 import { Test, TestingModule } from '@nestjs/testing';
 import { BrkController } from './brk.controller';
+import { BrkService } from './brk.service';
+
+const mockConfigService = {
+  get: (key: string) => {
+    return;
+  },
+};
 
 describe('BrkController', () => {
   let controller: BrkController;
@@ -7,6 +15,13 @@ describe('BrkController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BrkController],
+      providers: [
+        BrkService,
+        {
+          provide: ConfigService,
+          useValue: mockConfigService,
+        },
+      ],
     }).compile();
 
     controller = module.get<BrkController>(BrkController);
